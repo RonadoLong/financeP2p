3fa3e949a86cd03ed84e5cbba878156f43d0ee88
TorettoLong <managementsfzr@gmail.com>
2017-04-09 13:59:57 +0800

    "login bug"

--- finance-manage/finance-manage-dao/src/main/java/com/ming/finance/mapper/FinanceUserDao.java
+++ finance-manage/finance-manage-dao/src/main/java/com/ming/finance/mapper/FinanceUserDao.java
@@ -11,13 +11,21 @@ public interface FinanceUserDao {
 
     int deleteByExample(FinanceUserQuery example);
 
+    int deleteByPrimaryKey(Long sysUserId);
+
     int insert(FinanceUser record);
 
     int insertSelective(FinanceUser record);
 
     List<FinanceUser> selectByExample(FinanceUserQuery example);
 
+    FinanceUser selectByPrimaryKey(Long sysUserId);
+
     int updateByExampleSelective(@Param("record") FinanceUser record, @Param("example") FinanceUserQuery example);
 
     int updateByExample(@Param("record") FinanceUser record, @Param("example") FinanceUserQuery example);
+
+    int updateByPrimaryKeySelective(FinanceUser record);
+
+    int updateByPrimaryKey(FinanceUser record);
 }
